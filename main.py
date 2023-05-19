40da8c0ae874799938294427dc166fa38685ca64
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-19 13:57:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def index():
     nombre = request.form.get('nombre')
     return nombre
 
+@app.route('/streamlit')
+def streamlit():
+    st.set_page_config(page_title="My Streamlit App")
+    st.write("Hello, world!")
+
 if __name__ == '__main__':
     app.run()
