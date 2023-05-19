8722bd10874adabb18ebf31c59151841ad50f9c2
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-19 12:05:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
     return render_template('test.html')
-@app.route('/streamlit')
-def streamlit():
-    st.set_page_config(page_title="My Streamlit App")
-    st.write("Hello, world!")
+
 if __name__ == '__main__':
     app.run()
