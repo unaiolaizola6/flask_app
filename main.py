92c4fec6bfc9d0ece360b8e58dca258c5f9d625e
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-19 12:58:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 import streamlit as st
 
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
+
+@app.route('/')
 def index():
-    nombre = request.form.get('nombre')
-    return nombre
+    return "hello"
 
 if __name__ == '__main__':
     app.run()
