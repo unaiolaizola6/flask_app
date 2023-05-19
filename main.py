6c602c1ddc8c30216cf361cbeaeb4b0f309bc425
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-19 12:01:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import streamlit as st
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     return render_template('test.html')
 @app.route('/streamlit')
