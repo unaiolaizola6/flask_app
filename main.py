91719bb7d7fcd9fe3c898127c13663a3c9340cf9
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-22 10:56:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from flask import Flask, render_template
 import altair as alt
-from vega_datasets import data
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def index():
-    # Cargar los datos del conjunto de datos de ejemplos de Altair
-    cars = data.cars()
+    # Crear los datos
+    data = pd.DataFrame({
+        'x': [1, 2, 3, 4, 5],
+        'y': [3, 5, 2, 4, 6]
+    })
 
-    # Crear el gráfico de Altair
-    chart = alt.Chart(cars).mark_point().encode(
-        x='Horsepower',
-        y='Miles_per_Gallon',
-        color='Origin'
-    ).interactive()
+    # Crear el gráfico de líneas
+    chart = alt.Chart(data).mark_line().encode(
+        x='x',
+        y='y'
+    ).to_json()
 
-    # Guardar el gráfico en un archivo temporal
-    chart.save('chart.html')
-
-    # Renderizar la plantilla HTML que contiene el gráfico
-    return render_template('test.html')
+    return render_template('index.html', chart=chart)
 
 if __name__ == '__main__':
     app.run()
+
 
 
 
