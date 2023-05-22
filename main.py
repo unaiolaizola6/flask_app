95fc044f4635efce683a1a25a05ca28ff9e4432a
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-22 10:32:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Cargar los datos
+    # Cargar los datos del conjunto de datos de ejemplos de Altair
     cars = data.cars()
 
-    # Crear el gráfico con Altair
-    chart = alt.Chart(cars).mark_circle().encode(
+    # Crear el gráfico de Altair
+    chart = alt.Chart(cars).mark_point().encode(
         x='Horsepower',
         y='Miles_per_Gallon',
         color='Origin'
     ).interactive()
 
-    # Guardar el gráfico en un archivo HTML temporal
+    # Guardar el gráfico en un archivo temporal
     chart.save('chart.html')
 
-    # Renderizar la plantilla HTML con el gráfico
-    return render_template('test.html')
+    # Renderizar la plantilla HTML que contiene el gráfico
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
 
 
 
