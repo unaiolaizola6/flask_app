1cd7db77385de3f1e86934afbee0eaa1fb8995f2
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-22 09:07:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import streamlit as st
-from streamlit.server.server import Server
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-   return render_template('index.html')
+    nombre = request.form.get('nombre')
+    return nombre
 
 @app.route('/streamlit')
 def streamlit():
-   st.set_page_config(page_title="My Streamlit App")
-   st.write("Hello, world!")
+    st.set_page_config(page_title="My Streamlit App")
+    st.write("Hello, world!")
 
 if __name__ == '__main__':
-   server = Server(app)
-   server.start()
-   server.io_loop.start()
+    app.run()
+
